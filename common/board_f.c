b026015a340c43f55ffea4970670bb7855340bf1
Vincent Tinelli <vincent.tinelli@intel.com>
2014-03-21 15:12:43 +0100

    [UPSTREAM_CANDIDATE] add USE_FDT compilation flag
    Signed-off-by: Vincent Tinelli <vincent.tinelli@intel.com>

--- common/board_f.c
+++ common/board_f.c
@@ -280,7 +280,7 @@ __weak int arch_cpu_init(void)
 	return 0;
 }
 
-#ifdef CONFIG_OF_HOSTFILE
+#if defined(CONFIG_USE_FDT) && defined(CONFIG_OF_HOSTFILE)
 
 static int read_fdt_from_file(void)
 {
@@ -335,6 +335,7 @@ static int setup_ram_buf(void)
 }
 #endif
 
+#ifdef CONFIG_USE_FDT
 static int setup_fdt(void)
 {
 #ifdef CONFIG_OF_EMBED
@@ -354,6 +355,7 @@ static int setup_fdt(void)
 						(uintptr_t)gd->fdt_blob);
 	return 0;
 }
+#endif
 
 /* Get the top of usable RAM */
 __weak ulong board_get_usable_ram_top(ulong total_size)
@@ -549,6 +551,7 @@ static int reserve_global_data(void)
 	return 0;
 }
 
+#ifdef CONFIG_USE_FDT
 static int reserve_fdt(void)
 {
 	/*
@@ -567,6 +570,7 @@ static int reserve_fdt(void)
 
 	return 0;
 }
+#endif
 
 static int reserve_stacks(void)
 {
@@ -724,6 +728,7 @@ static int setup_dram_config(void)
 	return 0;
 }
 
+#ifdef CONFIG_USE_FDT
 static int reloc_fdt(void)
 {
 	if (gd->new_fdt) {
@@ -733,6 +738,7 @@ static int reloc_fdt(void)
 
 	return 0;
 }
+#endif
 
 static int setup_reloc(void)
 {
@@ -789,7 +795,9 @@ static init_fnc_t init_sequence_f[] = {
 	setup_ram_buf,
 #endif
 	setup_mon_len,
+#ifdef CONFIG_USE_FDT
 	setup_fdt,
+#endif
 	trace_early_init,
 #if defined(CONFIG_MPC85xx) || defined(CONFIG_MPC86xx)
 	/* TODO: can this go into arch_cpu_init()? */
@@ -798,12 +806,12 @@ static init_fnc_t init_sequence_f[] = {
 	arch_cpu_init,		/* basic arch cpu dependent setup */
 #ifdef CONFIG_X86
 	cpu_init_f,		/* TODO(sjg@chromium.org): remove */
-# ifdef CONFIG_OF_CONTROL
+#if defined(CONFIG_USE_FDT) && defined(CONFIG_OF_CONTROL)
 	find_fdt,		/* TODO(sjg@chromium.org): remove */
 # endif
 #endif
 	mark_bootstage,
-#ifdef CONFIG_OF_CONTROL
+#if defined(CONFIG_USE_FDT) && defined(CONFIG_OF_CONTROL)
 	fdtdec_check_fdt,
 #endif
 #if defined(CONFIG_BOARD_EARLY_INIT_F)
@@ -847,7 +855,7 @@ static init_fnc_t init_sequence_f[] = {
 #ifdef CONFIG_SANDBOX
 	sandbox_early_getopt_check,
 #endif
-#ifdef CONFIG_OF_CONTROL
+#if defined(CONFIG_USE_FDT) && defined(CONFIG_OF_CONTROL)
 	fdtdec_prepare_fdt,
 #endif
 	display_options,	/* say that we are here */
@@ -945,7 +953,9 @@ static init_fnc_t init_sequence_f[] = {
 #endif
 	setup_machine,
 	reserve_global_data,
+#ifdef CONFIG_USE_FDT
 	reserve_fdt,
+#endif
 	reserve_stacks,
 	setup_dram_config,
 	show_dram_config,
@@ -960,7 +970,9 @@ static init_fnc_t init_sequence_f[] = {
 	setup_board_extra,
 #endif
 	INIT_FUNC_WATCHDOG_RESET
+#ifdef CONFIG_USE_FDT
 	reloc_fdt,
+#endif
 	setup_reloc,
 #if !defined(CONFIG_ARM) && !defined(CONFIG_SANDBOX)
 	jump_to_copy,
