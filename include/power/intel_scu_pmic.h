27fbe5fb64b52e72c282eda5a37da5f02118b541
Vincent Tinelli <vincent.tinelli@intel.com>
2014-03-25 19:13:25 +0100

    scu/pmu: clean files
    Signed-off-by: Vincent Tinelli <vincent.tinelli@intel.com>

--- include/power/intel_scu_pmic.h
+++ include/power/intel_scu_pmic.h
@@ -1,11 +1,11 @@
 #ifndef __INTEL_SCU_PMIC_H__
 #define __INTEL_SCU_PMIC_H__
 
-int intel_scu_ipc_ioread8(u16 addr, u8 *data);
-int intel_scu_ipc_ioread32(u16 addr, u32 *data);
-int intel_scu_ipc_readv(u16 *addr, u8 *data, int len);
+int intel_scu_ipc_ioread8(u16 addr, u8 * data);
+int intel_scu_ipc_ioread32(u16 addr, u32 * data);
+int intel_scu_ipc_readv(u16 * addr, u8 * data, int len);
 int intel_scu_ipc_iowrite8(u16 addr, u8 data);
-int intel_scu_ipc_writev(u16 *addr, u8 *data, int len);
+int intel_scu_ipc_writev(u16 * addr, u8 * data, int len);
 int intel_scu_ipc_update_register(u16 addr, u8 data, u8 mask);
 
 extern void intel_pmic_init(void);
