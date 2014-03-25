7a78b0c8a29ec01eebc7c6548340b46a74d58433
Vincent Tinelli <vincent.tinelli@intel.com>
2014-03-25 10:37:50 +0100

    tangier: add mmc driver
    Signed-off-by: Vincent Tinelli <vincent.tinelli@intel.com>

--- arch/x86/include/asm/arch-tangier/mmc.h
+++ arch/x86/include/asm/arch-tangier/mmc.h
@@ -1,109 +1,11 @@
-/*
- * (C) Copyright 2009 SAMSUNG Electronics
- * Minkyu Kang <mk7.kang@samsung.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- */
-
-#ifndef __ASM_ARCH_MMC_H_
-#define __ASM_ARCH_MMC_H_
-
+#include <asm/types.h>
+#include <linux/types.h>
+#include <common.h>
+#include <malloc.h>
 #include <pci.h>
+#include <part.h>
+#include <mmc.h>
+#include <sdhci.h>
 
-#define SDHCI_CONTROL2		0x80
-#define SDHCI_CONTROL3		0x84
-#define SDHCI_CONTROL4		0x8C
-
-#define SDHCI_CTRL2_ENSTAASYNCCLR	(1 << 31)
-#define SDHCI_CTRL2_ENCMDCNFMSK		(1 << 30)
-#define SDHCI_CTRL2_CDINVRXD3		(1 << 29)
-#define SDHCI_CTRL2_SLCARDOUT		(1 << 28)
-
-#define SDHCI_CTRL2_FLTCLKSEL_MASK	(0xf << 24)
-#define SDHCI_CTRL2_FLTCLKSEL_SHIFT	(24)
-#define SDHCI_CTRL2_FLTCLKSEL(_x)	((_x) << 24)
-
-#define SDHCI_CTRL2_LVLDAT_MASK		(0xff << 16)
-#define SDHCI_CTRL2_LVLDAT_SHIFT	(16)
-#define SDHCI_CTRL2_LVLDAT(_x)		((_x) << 16)
-
-#define SDHCI_CTRL2_ENFBCLKTX		(1 << 15)
-#define SDHCI_CTRL2_ENFBCLKRX		(1 << 14)
-#define SDHCI_CTRL2_SDCDSEL		(1 << 13)
-#define SDHCI_CTRL2_SDSIGPC		(1 << 12)
-#define SDHCI_CTRL2_ENBUSYCHKTXSTART	(1 << 11)
-
-#define SDHCI_CTRL2_DFCNT_MASK(_x)	((_x) << 9)
-#define SDHCI_CTRL2_DFCNT_SHIFT		(9)
-
-#define SDHCI_CTRL2_ENCLKOUTHOLD	(1 << 8)
-#define SDHCI_CTRL2_RWAITMODE		(1 << 7)
-#define SDHCI_CTRL2_DISBUFRD		(1 << 6)
-#define SDHCI_CTRL2_SELBASECLK_MASK(_x)	((_x) << 4)
-#define SDHCI_CTRL2_SELBASECLK_SHIFT	(4)
-#define SDHCI_CTRL2_PWRSYNC		(1 << 3)
-#define SDHCI_CTRL2_ENCLKOUTMSKCON	(1 << 1)
-#define SDHCI_CTRL2_HWINITFIN		(1 << 0)
-
-#define SDHCI_CTRL3_FCSEL3		(1 << 31)
-#define SDHCI_CTRL3_FCSEL2		(1 << 23)
-#define SDHCI_CTRL3_FCSEL1		(1 << 15)
-#define SDHCI_CTRL3_FCSEL0		(1 << 7)
-
-#define SDHCI_CTRL4_DRIVE_MASK(_x)	((_x) << 16)
-#define SDHCI_CTRL4_DRIVE_SHIFT		(16)
-#define SDHCI_CONTROLLER_BASE_ADDR	CONFIG_SYS_EMMC_PORT_BASE
-
-int s5p_sdhci_init(u32 regbase, int index, int bus_width);
-
-static inline void set_mmc_clk(int dev_index, unsigned int div) {
-
-}
-
-static inline unsigned int s5p_mmc_init(int index, int bus_width)
-{
-	unsigned int base = 0;
-	u32 mmc_pci = PCI_BDF(0, 23, 0);	/* eMMC 4.5 */
-	pci_read_config_dword(mmc_pci, 0x10, &base);
-	base &= 0xffffe000;
-	if (base == 0xffffe000 || base == 0x0) {
-		printf("No eMMC4.5 BAR found in PCI\n");
-		mmc_pci = PCI_BDF(0, 16, 0);	/* eMMC 4.41 */
-		pci_read_config_dword(mmc_pci, 0x10, &base);
-		base &= 0xffffe000;
-	} else {
-		printf("eMMC4.5 BAR found in PCI(0x%08x)\n", base);
-		goto found;
-	}
-
-	if (base == 0xffffe000 || base == 0x0) {
-		printf("No eMMC4.41 BAR found in PCI\n");
-	} else {
-		printf("eMMC4.41 BAR found in PCI(0x%08x)\n", base);
-		goto found;
-	}
-
-	if (base == 0xffffffff || base == 0xffffe000 || base == 0x0) {
-		base = CONFIG_SYS_EMMC_PORT_BASE;
-		printf("No eMMC BAR found at all\n");
-		printf("Use hard-coded eMMC BAR(0x%08x)\n", CONFIG_SYS_EMMC_PORT_BASE);
-	}
-found:
-	base += (0x40000 * index);
-
-	return s5p_sdhci_init(base, index, bus_width);
-}
-#endif
+int tangier_mmc_init(struct mmc *mmc);
+int tangier_sdhci_init(u32 regbase, int index, int bus_width);
