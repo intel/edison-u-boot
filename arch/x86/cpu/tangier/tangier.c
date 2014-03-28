32bf6f4f60c849d39f9dd2b41577abd2aada01b1
scolleux <sebastienx.colleur@intel.com>
2014-03-28 15:22:20 +0100

    tangier: add tsc calibration specific implementation
    PCAT relies on single FSB frequency for tsc calibration
    
    tangier has different FSB values
    
    Signed-off-by: scolleux <sebastienx.colleur@intel.com>

--- arch/x86/cpu/tangier/tangier.c
+++ arch/x86/cpu/tangier/tangier.c
@@ -12,6 +12,8 @@
 #include <asm/io.h>
 #include <asm/errno.h>
 #include <asm/arch/mmc.h>
+#include <asm/msr.h>
+#include <asm/arch/intel-mid.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -58,4 +60,52 @@ int board_mmc_init(bd_t * bis)
 	unsigned int base = CONFIG_SYS_EMMC_PORT_BASE + (0x40000 * index);
 
 	return tangier_sdhci_init(base, index, 4);
+}
+
+
+/* ovveride get_tbclk_mhz code see tsc_timer */
+/* Get the speed of the TSC timer in MHz */
+unsigned __attribute__((no_instrument_function)) long get_tbclk_mhz(void)
+{
+	u32 ratio , bus_freq;
+	u64 platform_info = native_read_msr(MSR_PLATFORM_INFO);
+	u64 msr_fsb_freq = native_read_msr(MSR_FSB_FREQ);
+
+	/* compute and correct ratio if necessary */
+	ratio = ((platform_info >> 8) & 0xff);
+	if(!ratio)
+	{
+		ratio = 4;
+		debug("Read a zero ratio, force tsc ratio to 4 ...\n");
+	}
+
+	/* compute fsb */
+	bus_freq = (u32) (msr_fsb_freq & 0x7);
+	/* lookup real bus freq in kHz according to its index */
+	switch(bus_freq)
+	{
+	case 2:
+		bus_freq = FSB_FREQ_133SKU;
+		break;
+	case 3:
+		bus_freq = FSB_FREQ_167SKU;
+		break;
+	case 4:
+		bus_freq = FSB_FREQ_83SKU;
+		break;
+	case 5:
+		bus_freq = FSB_FREQ_400SKU;
+		break;
+	case 6:
+		bus_freq = FSB_FREQ_267SKU;
+		break;
+	case 7:
+		bus_freq = FSB_FREQ_333SKU;
+		break;
+	default:  /* handle also 0 and 1 */
+		bus_freq = FSB_FREQ_100SKU;
+		break;
+	}
+	// return Freq in Mhz
+	return ((bus_freq * ratio)/1000);
 }
