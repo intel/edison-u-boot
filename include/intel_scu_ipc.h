130c5a86e2a030e99a82255d4d07da6cd633a6ae
Vincent Tinelli <vincent.tinelli@intel.com>
2014-08-08 22:16:02 +0200

    hw_id: read hardware_id from scu using dedicated ipc
    Signed-off-by: Vincent Tinelli <vincent.tinelli@intel.com>

--- include/intel_scu_ipc.h
+++ include/intel_scu_ipc.h
@@ -6,6 +6,7 @@
 #define IPCMSG_COLD_RESET   0xF1
 #define IPCMSG_SOFT_RESET   0xF2
 #define IPCMSG_COLD_BOOT    0xF3
+#define IPCMSG_GET_FW_REVISION	0xF4
 #define IPCMSG_WATCHDOG_TIMER   0xF8	/* Set Kernel Watchdog Threshold */
 
 #define IPC_ERR_NONE            0
@@ -20,6 +21,17 @@
 #define IPC_CMD_VRTC_SETTIME      1	/* Set time */
 #define IPC_CMD_VRTC_SETALARM     2	/* Set alarm */
 #define IPC_CMD_VRTC_SYNC_RTC     3	/* Sync MSIC/PMIC RTC to VRTC */
+
+union ipc_ifwi_version {
+	u8 raw[16];
+	struct {
+		u16 ifwi_minor;
+		u8 ifwi_major;
+		u8 hardware_id;
+		u32 reserved[3];
+	} fw __attribute__((packed));
+} __attribute__((packed));
+
 /* Issue commands to the SCU with or without data */
 void intel_scu_ipc_send_command(u32 cmd);
 int intel_scu_ipc_check_status(void);
@@ -27,9 +39,9 @@ int intel_scu_ipc_simple_command(int cmd, int sub);
 void intel_scu_ipc_lock(void);
 void intel_scu_ipc_unlock(void);
 int intel_scu_ipc_command(u32 cmd, u32 sub, u8 * in, u8 inlen,
-			  u32 * out, u32 outlen);
+		u32 * out, u32 outlen);
 int intel_scu_ipc_raw_cmd(u32 cmd, u32 sub, u8 * in, u8 inlen,
-			  u32 * out, u32 outlen, u32 dptr, u32 sptr);
+		u32 * out, u32 outlen, u32 dptr, u32 sptr);
 
 
 void ipc_data_writel(u32 data, u32 offset);	/* Write ipc data */
@@ -37,7 +49,7 @@ u32 ipc_read_status(void);
 u8 ipc_data_readb(u32 offset);	/* Read ipc byte data */
 u32 ipc_data_readl(u32 offset);	/* Read ipc u32 data */
 int intel_scu_ipc_command(u32 cmd, u32 sub, u8 * in, u8 inlen, u32 * out,
-			  u32 outlen);
+		u32 outlen);
 int init_scu_ipc(void);
 
 enum intel_mid_cpu_type {
