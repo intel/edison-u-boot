b70ee5afab65e78683a4340a3dc7ad18d37d0895
Scott D Phillips <scott.d.phillips@intel.com>
2015-09-30 11:07:36 -0700

    edison: Enable fastboot
    This isn't working yet, bulk transfer on ep1in-bulk hangs.

--- board/intel/edison/edison.c
+++ board/intel/edison/edison.c
@@ -1,4 +1,5 @@
 #include <common.h>
+#include <dwc3-uboot.h>
 #include <asm/cache.h>
 #include <usb.h>
 #include <linux/usb/gadget.h>
@@ -23,9 +24,34 @@ enum {
 
 #define IPC_CMD(cmd, sub) (sub << 12 | cmd)
 
+static struct dwc3_device dwc3_device_data = {
+	.maximum_speed = USB_SPEED_HIGH,
+	.base = CONFIG_SYS_USB_OTG_BASE,
+	.dr_mode = USB_DR_MODE_PERIPHERAL,
+	.index = 0,
+};
+
+int usb_gadget_handle_interrupts(int controller_index)
+{
+	dwc3_uboot_handle_interrupt(controller_index);
+	return 0;
+}
+
 int board_usb_init(int index, enum usb_init_type init)
 {
-	return usb_gadget_init_udc();
+	if (index == 0 && init == USB_INIT_DEVICE)
+		return dwc3_uboot_init(&dwc3_device_data);
+	else
+		return -EINVAL;
+}
+
+int board_usb_cleanup(int index, enum usb_init_type init)
+{
+	if (index == 0 && init == USB_INIT_DEVICE) {
+		dwc3_uboot_exit(index);
+		return 0;
+	} else
+		return -EINVAL;
 }
 
 void watchdog_reset(void)
