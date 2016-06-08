a5ecd9896932240662283377c9c821566888b68d
Sanrio Alvares <sanrio.alvares@intel.com>
2016-06-08 10:54:33 -0700

    Allow both signed and unsigned uboot image builds
    If U-boot binaries for edison are needed to be signed,
    please build using --sign.
    
    Change-Id: I040f162d04e53024dea7df08fb48df5c283a4f95
    Signed-off-by: Sanrio Alvares <sanrio.alvares@intel.com>
    Reviewed-on: https://android.intel.com/515401
    Reviewed-by: Beare, Bruce J <bruce.j.beare@intel.com>

--- build_for_brillo.sh
+++ build_for_brillo.sh
@@ -1,4 +1,22 @@
+#!/bin/bash
+
 set -e
+
+if [[ -z "${1// }" ]]
+then
+    echo "To sign u-boot, run build_for_brillo.sh --sign."
+    echo "Building u-boot..."
+    sleep 2
+else
+if [ "$1" == "--sign" ]
+then
+    echo "Building u-boot for signing..."
+    sleep 2
+else
+    echo "To sign u-boot, run build_for_brillo.sh --sign"
+    exit
+fi
+fi
 
 rm -rf out
 
@@ -7,14 +25,25 @@ make O=out CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilts/gcc/linux-x86/x86/x86_64-l
 
 rm -rf out-fastboot
 
-make O=out-fastboot CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9/bin/x86_64-linux-android- CFLAGS=-m32 KCPPFLAGS=-DCONFIG_BRILLO_FASTBOOT_ONLY -j$(nproc) edison_defconfig
-make O=out-fastboot CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9/bin/x86_64-linux-android- CFLAGS=-m32 KCPPFLAGS=-DCONFIG_BRILLO_FASTBOOT_ONLY -j$(nproc)
+if [ "$1" == "--sign" ]
+then
+    make O=out-fastboot CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9/bin/x86_64-linux-android- CFLAGS=-m32 KCPPFLAGS=-DCONFIG_BRILLO_FASTBOOT_ONLY KCPPFLAGS=-DCONFIG_OSIP_SIGNED_ATTRIBUTE -j$(nproc) edison_defconfig
+    make O=out-fastboot CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9/bin/x86_64-linux-android- CFLAGS=-m32 KCPPFLAGS=-DCONFIG_BRILLO_FASTBOOT_ONLY KCPPFLAGS=-DCONFIG_OSIP_SIGNED_ATTRIBUTE -j$(nproc)
+else
+    make O=out-fastboot CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9/bin/x86_64-linux-android- CFLAGS=-m32 KCPPFLAGS=-DCONFIG_BRILLO_FASTBOOT_ONLY -j$(nproc) edison_defconfig
+    make O=out-fastboot CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9/bin/x86_64-linux-android- CFLAGS=-m32 KCPPFLAGS=-DCONFIG_BRILLO_FASTBOOT_ONLY -j$(nproc)
+fi
 
 dd if=out/u-boot.bin of=u-boot-edison.bin bs=4096 seek=1
-
-openssl base64 -d <<EOF | xz -d > u-boot-edison.img
-/Td6WFoAAATm1rRGAgAhARYAAAB0L+Wj4AH/ADJdABITxmI/dj2HEN524/dYfdgu
-VSVrkQEE6XSgsZnwdRltLRkU6GTigpK5eAw3CQAaRzcAAAAAtBe6DKrPzUcAAU6A
-BAAAAASYg8ixxGf7AgAAAAAEWVo=
+if [ "$1" == "--sign" ]
+then
+    dd if=out-fastboot/u-boot.bin of=u-boot-edison.img obs=4096 seek=1
+    echo "Use Image Studio Tool to sign u-boot-edison.bin and u-boot-edison.img."
+else
+    openssl base64 -d <<EOF | xz -d > u-boot-edison.img
+    /Td6WFoAAATm1rRGAgAhARYAAAB0L+Wj4AH/ADJdABITxmI/dj2HEN524/dYfdgu
+    VSVrkQEE6XSgsZnwdRltLRkU6GTigpK5eAw3CQAaRzcAAAAAtBe6DKrPzUcAAU6A
+    BAAAAASYg8ixxGf7AgAAAAAEWVo=
 EOF
-dd if=out-fastboot/u-boot.bin of=u-boot-edison.img bs=4608 seek=1
+    dd if=out-fastboot/u-boot.bin of=u-boot-edison.img bs=4608 seek=1
+fi
